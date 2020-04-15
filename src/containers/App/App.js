17440f970640f49a3dd3b24694e40e437b7ef644
Jonathan Tschida <jonathan.tschida@outlook.com>
2020-04-15 16:39:00 -0600

    Add functionality to dynamically generate buttons for Browse

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,27 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { getSeries } from '../../actions';
+import { connect } from 'react-redux';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Header from '../../components/Header/Header';
 import Collection from '../Collection/Collection';
 import Browse from '../Browse/Browse';
 
-function App() {
-  return (
-    <main>
-      <Header />
-      <Switch>
-        <Route exact path='/'>
-          <Redirect to='/collection' />
-        </Route>
-        <Route path='/collection'>
-          <Collection />
-        </Route>
-        <Route path='/browse'>
-          <Browse />
-        </Route>
-      </Switch>
-    </main>
-  );
+class App extends Component {
+  componentDidMount() {
+    fetch('https://www.amiiboapi.com/api/amiiboseries')
+      .then(response => response.json())
+      .then(data => {
+        this.props.getSeries(data.amiibo);
+      })
+      .catch(error => console.error(error.message))
+  }
+
+  render() {
+    return (
+      <main>
+        <Header />
+        <Switch>
+          <Route exact path='/'>
+            <Redirect to='/collection' />
+          </Route>
+          <Route path='/collection'>
+            <Collection />
+          </Route>
+          <Route path='/browse'>
+            <Browse />
+          </Route>
+        </Switch>
+      </main>
+    );
+  }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+  getSeries: (series) => dispatch( getSeries(series) )
+});
+
+export default connect(null, mapDispatchToProps)(App);
--- src/containers/Browse/Browse.js
+++ src/containers/Browse/Browse.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './Browse.css';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-function Browse() {
+function Browse({ series }) {
+  const seriesButtons = series.map(series => {
+    return <Link to={`series/${series}`}>{series}</Link>
+  })
   return (
     <div>
       <h2>Browse</h2>
+      {seriesButtons}
     </div>
   )
 }
 
-export default Browse;
+const mapStateToProps = (state) => ({
+  series: Object.keys(state.cache)
+})
+
+export default connect(mapStateToProps)(Browse);
