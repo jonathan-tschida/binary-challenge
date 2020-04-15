f90a0372685b8d1ae8cb62747ade575df2870d9e
Jonathan Tschida <jonathan.tschida@outlook.com>
2020-04-15 15:03:53 -0600

    Add placeholder for empty collection

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Header from '../../components/Header/Header';
+import Collection from '../Collection/Collection';
 
 function App() {
   return (
     <main>
       <Header />
+      <Switch>
+        <Route exact path='/'>
+          <Redirect to='/collection' />
+        </Route>
+        <Route path='/collection'>
+          <Collection />
+        </Route>
+        <Route path='/browse'>
+        </Route>
+      </Switch>
     </main>
   );
 }
---
+++ src/containers/Collection/Collection.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Collection.css';
+import { Link } from 'react-router-dom';
+
+const Collection = () => {
+  return (
+    <section>
+      <h2>Oops!</h2>
+      <p>Looks like you haven't added anything to your collection yet! Click browse to get started!</p>
+      <Link to='/browse'>Browse</Link>
+    </section>
+  )
+}
+
+export default Collection;
