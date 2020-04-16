b25fb012952dc66f1bc740d09897543406863cc0
Jonathan Tschida <jonathan.tschida@outlook.com>
2020-04-16 15:09:07 -0600

    Connect Collection to the store to find figure data from collection ids

--- src/containers/Collection/Collection.js
+++ src/containers/Collection/Collection.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Collection.css';
 import { Link } from 'react-router-dom';
+import { toggleCollected } from '../../actions';
+import { connect } from 'react-redux';
 
 function Collection() {
   return (
@@ -13,4 +15,18 @@ function Collection() {
   )
 }
 
-export default Collection;
+const mapStateToProps = (state) => {
+  let cachedFigures = Object.keys(state.cache).flatMap(series => {
+    return state.cache[series].figures
+  });
+  let collectedFigures = state.collection.map(id => {
+    return cachedFigures.find(figure => figure.id === id)
+  })
+  return { figures: collectedFigures }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  toggleCollected: (id) => dispatch( toggleCollected(id) )
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Collection);
