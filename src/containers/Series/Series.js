226a63a69b321b37fcee774fdd09c2896cae2eae
Jonathan Tschida <jonathan.tschida@outlook.com>
2020-04-16 14:44:31 -0600

    Add functionality to import and render amiibo cards

--- src/containers/Series/Series.js
+++ src/containers/Series/Series.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './Series.css';
+import Amiibo from '../Amiibo/Amiibo';
 import { connect } from 'react-redux';
 import { getSeriesData } from '../../actions';
 
 class Series extends Component {
   componentDidMount() {
     let { series } = this.props.match.params;
-    fetch('https://www.amiiboapi.com/api/amiibo/?amiiboSeries=' + series)
+    fetch('https://www.amiiboapi.com/api/amiibo/?type=figure&amiiboSeries=' + series)
       .then(response => response.json())
       .then(data => {
         this.props.getSeriesData(series, data.amiibo)
@@ -16,8 +17,17 @@ class Series extends Component {
 
   render() {
     let { series } = this.props.match.params;
+    let { figures } = this.props;
+    let amiiboCards = figures.map(figure => {
+      return <Amiibo {...figure} key={figure.id} />
+    })
     return (
-      <h1>{series}</h1>
+      <section>
+        <h1>{series}</h1>
+        <div>
+          {amiiboCards.length ? amiiboCards : <p>Loading...</p>}
+        </div>
+      </section>
     )
   }
 }
@@ -26,4 +36,13 @@ const mapDispatchToProps = (dispatch) => ({
   getSeriesData: (series, data) => dispatch( getSeriesData(series, data) )
 })
 
-export default connect(null, mapDispatchToProps)(Series);
+const mapStateToProps = (state, ownProps) => {
+  let { series } = ownProps.match.params;
+  let seriesCache = state.cache[series] || {};
+  let figures = seriesCache.figures || [];
+  return {
+    figures
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Series);
