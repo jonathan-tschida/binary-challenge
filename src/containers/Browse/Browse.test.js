46c6d031a08a9011b2aee13e0fc3f6fa1fade349
Jonathan Tschida <jonathan.tschida@outlook.com>
2020-04-19 14:54:09 -0600

    Add test for Browse

---
+++ src/containers/Browse/Browse.test.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { render, fireEvent, waitFor } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import Browse from './Browse';
+import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from '../../reducers';
+import { createMemoryHistory } from 'history';
+
+function renderBrowse(testState) {
+  const testStore = createStore(rootReducer, testState);
+  const history = createMemoryHistory();
+  return render (
+    <Provider store={testStore}>
+      <Router history={history}>
+        <Browse />
+      </Router>
+    </Provider>
+  )
+}
+
+describe('Browse', () => {
+  it('renders correctly', () => {
+    const testState = {
+      cache: {
+        'Super Smash Bros.': {
+          figures: []
+        },
+        'Legend of Zelda': {
+          figures: []
+        }
+      }
+    }
+    const { getByText } = renderBrowse(testState);
+
+    expect(getByText('Super Smash Bros.')).toBeInTheDocument();
+    expect(getByText('Legend of Zelda')).toBeInTheDocument();
+  });
+
+  it('renders a Series after clicking on its link', () => {
+    const testState = {
+      cache: {
+        'Super Smash Bros.': {
+          figures: []
+        },
+        'Legend of Zelda': {
+          figures: []
+        }
+      }
+    }
+    const { getByText, getAllByText } = renderBrowse(testState);
+
+    fireEvent.click(getByText('Super Smash Bros.'));
+
+    expect(getAllByText('Super Smash Bros.')[1]).toBeInTheDocument();
+  })
+})
