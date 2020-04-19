8d993cafe1f153e20ec04c145491cd37655c23fc
Jonathan Tschida <jonathan.tschida@outlook.com>
2020-04-19 13:58:11 -0600

    Add test for Header

---
+++ src/components/Header/Header.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import Header from './Header';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+describe('Header', () => {
+  it('renders correctly', () => {
+    const { getByAltText, getByText } = render (
+      <Router>
+        <Header />
+      </Router>
+    )
+
+    expect(getByAltText('amiibo depot logo')).toBeInTheDocument();
+    expect(getByText('Browse')).toBeInTheDocument();
+    expect(getByText('Collection')).toBeInTheDocument();
+  })
+})
